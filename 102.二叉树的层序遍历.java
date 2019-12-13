6727b3f4f2c1679aaf9cf05215853069b73cb206
sss-0916 <sss-0916@qq.com>
2019-12-13 09:53:21 +0800

    广度优先搜索

---
+++ 102.二叉树的层序遍历.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int qSize = queue.size();
+            List<Integer> curLevel = new ArrayList<>();
+            for (int i = 0; i < qSize; ++i) {
+                TreeNode curNode = queue.poll();
+                curLevel.add(curNode.val);
+
+                if (curNode.left != null) {
+                    queue.offer(curNode.left);
+                }
+                if (curNode.right != null) {
+                    queue.offer(curNode.right);
+                }
+            }
+            result.add(curLevel);
+        }
+
+        return result;
+    }
+}
